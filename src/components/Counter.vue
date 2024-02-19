4afdc3778668983c08603b143081d8d8a4a798a4
bruno_aguirre <boguirre@gmail.com>
2024-02-19 00:19:41 -0500

    nuevo comienzo

---
+++ src/components/Counter.vue
@@ -0,0 +1,64 @@
+<template>
+  <h2>{{customTitle}}</h2>
+  <p>{{counter}} <sup>2</sup> = {{ counter * counter }}</p>
+  <div>
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        start: {
+            type: Number,
+            default: 100,
+            validator(value){
+                return value > 100
+            }
+        }
+    },
+
+    data(){
+        return{
+            counter: this.start
+        }
+    },
+    methods: {
+        getSquareValue(){
+            return this.counter * this.counter
+        },
+        increase(){
+            this.counter++
+        },
+        decrease(){
+            this.counter--
+        }
+    },
+    computed:{
+        squareCounter(){
+            return this.counter * this.counter
+        },
+        customTitle(){
+            return this.title ? this.title : 'Counter'
+        }
+    }
+};
+</script>
+
+<style>
+    button{
+        background-color: #64BB87;
+        border-radius: 5px;
+        color: white;
+        cursor: pointer;
+        padding: 5px 15px;
+        transition: 0.3s ease-in-out;
+        margin: 0.5px;
+    }
+    button:hover{
+        background-color: #5aa67b;
+        transition: 0.3s ease-in-out;
+    }
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pluginOptions: {
+    vite: {
+      build: {
+        rollupOptions: {
+          vuePluginOptions: {
+            __VUE_PROD_DEVTOOLS__: true,
+            __VUE_PROD_HYDRATION_MISMATCH__: false,
+            __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: true
+          }
+        }
+      }
+    }
+  }
 })
+
